f260ee88bbd8bc952a787dfefdcc6bc3fed440ed
Hristo Koev <hkoev@c1r4s1.42prague.com>
2023-01-15 15:27:05 +0100

    loop after exceed 256

--- libft/ft_memchr.c
+++ libft/ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: hkoev <hkoev@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/10 19:47:37 by hkoev             #+#    #+#             */
-/*   Updated: 2023/01/15 08:40:13 by hkoev            ###   ########.fr       */
+/*   Updated: 2023/01/15 15:23:07 by hkoev            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ void	*ft_memchr(const void *s, int c, size_t n)
 	const char	*ps;
 
 	ps = s;
+	if (c >= 256)
+		c = c % 256;
 	while (n--)
 	{
 		if (*ps == c)
