d4ba359ca708a6adf3828ec06fe55d24782b75cf
hristokoev <hkoev@DESKTOP-49TMISQ.localdomain>
2023-01-15 08:36:25 +0100

    readibility improv

--- libft/ft_strncmp.c
+++ libft/ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: hkoev <hkoev@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/09 12:59:20 by hkoev             #+#    #+#             */
-/*   Updated: 2023/01/11 21:21:28 by hkoev            ###   ########.fr       */
+/*   Updated: 2023/01/15 08:33:34 by hkoev            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,17 +15,16 @@
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
 	int	i;
+	int	a;
+	int	b;
 
 	i = 0;
 	while (n--)
 	{
-		if ((unsigned char)s1[i] != (unsigned char)s2[i])
-		{
-			if ((unsigned char)s1[i] < (unsigned char)s2[i])
-				return (-1);
-			else
-				return (1);
-		}
+		a = (unsigned char)s1[i];
+		b = (unsigned char)s2[i];
+		if (a != b)
+			return (a - b);
 		i++;
 	}
 	return (0);
