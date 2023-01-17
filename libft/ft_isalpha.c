4625142494881498265cb19e1bd07876769f1783
Hristo Koev <hkoev@c1r4s1.42prague.com>
2023-01-17 18:02:39 +0100

    remove main func

--- libft/ft_isalpha.c
+++ libft/ft_isalpha.c
@@ -6,22 +6,15 @@
 /*   By: hkoev <hkoev@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/09 12:32:34 by hkoev             #+#    #+#             */
-/*   Updated: 2023/01/15 16:07:36 by hkoev            ###   ########.fr       */
+/*   Updated: 2023/01/17 18:02:23 by hkoev            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdio.h>
 
 int	ft_isalpha(int c)
 {
 	if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'))
 		return (1);
-	return (0);
-}
-
-int	main(void)
-{
-	printf("%d \n", ft_isalpha(32));
 	return (0);
 }
