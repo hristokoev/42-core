afe082b2dc228cac6e9bd3256fcd6aacf3f4f622
Hristo Koev <hkoev@c1r4s1.42prague.com>
2023-01-17 18:00:54 +0100

    test

--- libft/ft_isalpha.c
+++ libft/ft_isalpha.c
@@ -6,15 +6,22 @@
 /*   By: hkoev <hkoev@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/09 12:32:34 by hkoev             #+#    #+#             */
-/*   Updated: 2023/01/10 20:16:02 by hkoev            ###   ########.fr       */
+/*   Updated: 2023/01/15 16:07:36 by hkoev            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdio.h>
 
 int	ft_isalpha(int c)
 {
 	if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'))
 		return (1);
+	return (0);
+}
+
+int	main(void)
+{
+	printf("%d \n", ft_isalpha(32));
 	return (0);
 }
