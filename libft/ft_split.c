27ea39683c404bab013e2c9af203aadc2c15737b
hristokoev <hkoev@DESKTOP-49TMISQ.localdomain>
2023-01-20 23:50:12 +0100

    ft_split - wip

--- libft/ft_split.c
+++ libft/ft_split.c
@@ -6,7 +6,7 @@
 /*   By: hkoev <hkoev@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/13 18:19:30 by hkoev             #+#    #+#             */
-/*   Updated: 2023/01/19 15:39:36 by hkoev            ###   ########.fr       */
+/*   Updated: 2023/01/20 21:53:09 by hkoev            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,51 @@
 // str = "lorem", "ipsum", "dolor", "sit", "amet"
 
 #include "libft.h"
+#include <stdio.h>
 
-char	**ft_split(char const *s, char c);
+int     ft_words(char const *s, char c)
+{
+        int     n;
+        char    prevc;
+
+        n = 0;
+        prevc = c;
+        while (*s)
+        {
+                if (*s != c && prevc == c)
+                        n++;
+                prevc = *s;                        
+                s++;
+        }
+        return (n);
+}
+
+size_t	ft_strlen(const char *str)
+{
+	size_t	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+char	**ft_split(char const *s, char c)
+{
+        char    **strings;
+        int     words;
+        int     len;
+        int     i;
+
+        words = ft_words(s, c);
+        strings = (char **)malloc(words * sizeof(char *));
+        i = 0;
+
+        return (strings);
+}
+
+int     main()
+{
+        printf("%d \n", ft_words("lorem ipsum dolor sit amet", ' '));
+        return (0);
+}
