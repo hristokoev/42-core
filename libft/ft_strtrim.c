8d8bb07256f82c358a21eba688689147e11c1a18
Hristo Koev <hkoev@c1r4s1.42prague.com>
2023-01-24 19:47:51 +0100

    fixed loopin through string

--- libft/ft_strtrim.c
+++ libft/ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: hkoev <hkoev@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/13 18:19:08 by hkoev             #+#    #+#             */
-/*   Updated: 2023/01/23 14:48:33 by hkoev            ###   ########.fr       */
+/*   Updated: 2023/01/24 19:34:15 by hkoev            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,18 +15,24 @@
 char	*ft_strtrim(char const *str, char const *set)
 {
 	char	*s;
+	int		strlen;
+	int		setlen;
 	int		start;
 	int		end;
 
+	strlen = ft_strlen(str);
+	setlen = ft_strlen(set);
 	start = 0;
-	end = ft_strlen(str) - 1;
+	end = ft_strlen(str);
 	if (!str || !set)
 		return (NULL);
-	while (ft_strchr(set, str[start]))
+	while (ft_strchr(set, str[start]) && strlen && setlen)
 		start++;
-	while (ft_strchr(set, str[end]))
+	while (ft_strchr(set, str[end]) && strlen && setlen)
 		end--;
-	s = malloc(sizeof(char) * end - start + 2);
+	if (start > end)
+		return ((char *)ft_calloc(1, sizeof(char)));
+	s = (char *)ft_calloc(end - start + 2, sizeof(char));
 	if (!s)
 		return (NULL);
 	ft_strlcpy(s, &str[start], end - start + 2);
